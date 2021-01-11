6031c8012f3e4aa45899b8a3b81304a10347e066
Sam <sam0000@live.nl>
2021-01-11 11:39:52 +0100

    back to top knop toegevoegd

--- script.js
+++ script.js
@@ -32,3 +32,20 @@ $(document).ready(function () {
 		}]
 	});
 });
+
+//------------- Back to top button -----------------------------
+
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
